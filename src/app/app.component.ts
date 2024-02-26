b4791478cf685c0543c4cacf647a172bea275254
victeo <victor.atomo@gmail.com>
2024-02-26 16:20:33 -0300

    adding basic settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        ButtonModule,
+        RouterOutlet,
+        DialogModule,
+
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'global-kardec-online';
+    title = 'global-kardec-online';
+    visible: boolean = false;
+
+    showDialog() {
+        this.visible = true;
+    }
 }
