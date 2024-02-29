e671913506f6e9f8373c5110ad9971ddf7944c93
victeo <victor.atomo@gmail.com>
2024-02-29 17:02:46 -0300

    [GKO-2] - Código do navbar separado

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,44 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Sidebar } from 'primeng/sidebar';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { MenubarModule } from 'primeng/menubar';
-import { MenuModule } from 'primeng/menu';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
 
 
 import { SidebarComponent } from '../../components/admin/dashboard/sidebar/sidebar.component';
+import { NavbarComponent } from '../../components/admin/dashboard/navbar/navbar.component';
 
 @Component({
     selector: 'app-admin-home',
     standalone: true,
-    imports: [SidebarModule, ButtonModule, MenuModule, MenubarModule, SidebarComponent],
+    imports: [SidebarComponent, NavbarComponent],
     templateUrl: './admin-home.component.html',
     styleUrl: './admin-home.component.less'
 })
-export class AdminHomeComponent implements OnInit {
-    @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+export class AdminHomeComponent  {
 
-    items: MenuItem[] | undefined;
-    visible: any = true;
-    cols: any;
 
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            }
-        ];
-       
-    }
-    closeCallback(e: Event): void {
-        this.sidebarRef.close(e);
-    }
-
-    sidebarVisible: boolean = true;
 }
--- src/app/components/admin/dashboard/navbar/navbar.component.ts
+++ src/app/components/admin/dashboard/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.less'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [MenubarModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.less'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+    items: MenuItem[] | undefined;
+
+    ngOnInit(): void {
+        this.items = [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            }
+        ];
+    }
 
 }
