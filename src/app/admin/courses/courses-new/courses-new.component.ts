326d6a845c3197ce34b55f6d1e7f4daf8d273fc0
victeo <victor.atomo@gmail.com>
2024-03-15 17:33:53 -0300

    [GKO-9] - Adicionado alguns mixins uteis e layout do curso

--- src/app/admin/courses/courses-new/courses-new.component.ts
+++ src/app/admin/courses/courses-new/courses-new.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+
 
 @Component({
-  selector: 'app-courses-new',
-  standalone: true,
-  imports: [],
-  templateUrl: './courses-new.component.html',
-  styleUrl: './courses-new.component.less'
+    selector: 'app-courses-new',
+    standalone: true,
+    imports: [ButtonModule, InputTextModule, InputTextareaModule],
+    templateUrl: './courses-new.component.html',
+    styleUrl: './courses-new.component.less'
 })
-export class CoursesNewComponent {
+export class CoursesNewComponent implements OnInit {
+    formGroup: FormGroup | undefined;
+
+    ngOnInit(): void {
+        this.formGroup = new FormGroup({
+            name: new FormControl<string | null>(null),
+            institute: new FormControl<string | null>(null),
+            language: new FormControl<string | null>(null),
+            description: new FormControl<string | null>(null)
+        });
+    }
 
 }
