780c6403187d043c1216cce1baa1a985c1d8f4a3
victeo <victor.atomo@gmail.com>
2024-04-03 16:45:22 -0300

    [GKO-9] - add logica de adicionar e remover conteúdos das aulas.

--- src/app/components/admin/tab-view/tab-view.component.ts
+++ src/app/components/admin/tab-view/tab-view.component.ts
@@ -1,5 +1,5 @@
 // Importações essenciais do Angular
-import { Component, OnInit } from '@angular/core'; // Funcionalidades básicas para componentes
+import { Component, OnInit, NgModule, ChangeDetectorRef } from '@angular/core'; // Funcionalidades básicas para componentes
 import { CommonModule } from '@angular/common'; // Funcionalidades básicas do Angular
 
 // Importações do PrimeNG para componentes específicos
@@ -8,6 +8,8 @@ import { ButtonModule } from 'primeng/button';    // Componente Button do PrimeN
 import { DialogModule } from 'primeng/dialog';  // Componente Dialog do PrimeNG
 import { InputTextModule } from 'primeng/inputtext'; // Componente InputText do PrimeNG
 import { InputTextareaModule } from 'primeng/inputtextarea'; // Componente InputTextarea do PrimeNG
+import { DropdownModule } from 'primeng/dropdown'; // Componente Dropdown do PrimeNG
+
 // ... outros imports do PrimeNG conforme a necessidade
 
 // Importações relacionadas a formulários do Angular
@@ -27,7 +29,9 @@ import { NgForm, FormControl } from '@angular/forms'; // Classes para formulári
         DialogModule,
         InputTextModule,
         InputTextareaModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        DropdownModule,
+        FormsModule
 
     ],
     templateUrl: './tab-view.component.html',
@@ -72,17 +76,15 @@ export class TabViewComponent implements OnInit {
     formControl!: FormGroup;
 
     /**
+     * Construtor da classe para inicialização do FormBuilder e ChangeDetectorRef.
      * 
-     * @param formBuilder 
+     * @param formBuilder Objeto FormBuilder utilizado para construir formGroups e formControls.
+     * @param cdr Objeto ChangeDetectorRef para forçar detecção de alterações.
      */
 
     constructor(
-        /**
-         * Construtor da classe para inicialização do FormBuilder.
-         * 
-         * @formBuilder Objeto FormBuilder utilizado para construir formGroups e formControls.
-         */
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private cdr: ChangeDetectorRef
     ) {
         // Verifica se existe um array armazenado em cache
         const classCache = localStorage.getItem('classCache');
@@ -95,24 +97,63 @@ export class TabViewComponent implements OnInit {
 
     }
 
+    /**
+      * Array que contém as opções de tipo de arquivo.
+      * Cada opção é um objeto com propriedades 'name' e 'code'.
+      * 
+      * @optionsType Array que contém as opções de tipo de arquivo.
+      *               Cada opção é um objeto com as propriedades 'name' e 'code'.
+      *               O tipo de arquivo é representado pelo nome e pelo código do ícone correspondente.
+      */
+    optionsType: any[] | undefined;
 
+    /**
+     * Método de ciclo de vida do Angular chamado após a inicialização do componente.
+     * Inicializa o FormGroup utilizando o FormBuilder para criar formControls para os campos do formulário.
+     * Também inicializa as opções de tipo de arquivo.
+     */
     ngOnInit(): void {
         // Inicialização do FormGroup usando o formBuilder para criar formControls
         this.formControl = this.formBuilder.group({
             name: new FormControl('', [Validators.required]),
+            typeFile: new FormControl('', [Validators.required]),
             link: new FormControl('', [Validators.required]),
             institute: new FormControl('', [Validators.required]),
             sort: new FormControl('', [Validators.required]),
             description: new FormControl('', [Validators.required])
         });
+
+        // Inicialização das opções de arquivo
+        this.optionsType = [
+            { name: 'Arquivo', code: 'pi-file' },
+            { name: 'Slide Aula', code: 'pi-id-card' },
+            { name: 'Áudio', code: 'pi-volume-up' },
+            { name: 'Vídeo', code: 'pi-play' }
+        ];
+        console.log(this.resources);
     }
 
     visible: boolean = false;
+    indexContent: any = "";
 
-    showDialog() {
+    /**
+     * Método responsável por exibir o diálogo de adicionar conteúdo.
+     * 
+     * @param index O índice do conteúdo para o qual o diálogo está sendo exibido.
+     */
+    showDialog(index: any): void {
+        this.indexContent = index;
         this.visible = true;
     }
 
+    /**
+     * Método responsável por adicionar um novo curso ao array de recursos.
+     * Cria um novo objeto com um ID único e um array de conteúdo vazio, e adiciona-o ao array de recursos.
+     * Incrementa o próximo ID para a próxima inserção.
+     * Salva o array no cache do navegador.
+     * 
+     * @remarks A saída do console exibe o array de recursos atualizado após a adição do novo curso.
+     */
     addCourse(): void {
         this.resources.push()
 
@@ -146,17 +187,42 @@ export class TabViewComponent implements OnInit {
         console.log(this.resources)
     }
 
+    /**
+   * Método responsável por limpar os valores dos campos do formulário.
+   */
     clearForm() {
-        // Limpa os valores dos campos do formulário
+        // Define os valores dos campos do formulário como vazio
+        this.formControl.get('name')?.setValue('');
+        this.formControl.get('typeFile')?.setValue('');
+        this.formControl.get('link')?.setValue('');
+        this.formControl.get('institute')?.setValue('');
+        this.formControl.get('sort')?.setValue('');
+        this.formControl.get('description')?.setValue('');
     }
 
     submitForm(): void {
-
         if (this.formControl && this.formControl.valid) {
             // Lógica para enviar o formulário
-            console.log(this.formControl.value);
+            if (this.indexContent !== "") {
+                //  this.resources[this.indexContent].content.push(this.formControl.value);
+                this.resources[this.indexContent].content.push(this.formControl.value);
+                localStorage.setItem('classCache', JSON.stringify(this.resources));
+                this.visible = false;
+                this.clearForm()
+            }
         } else {
             // Tratar caso o formulário seja inválido
+        }
+    }
+
+    onCloseTab(tabId: string) {
+        // Encontrar o índice do item no array resources com base no tabId
+        const index = this.resources.findIndex(item => item.id === tabId);
+        // Se o índice for encontrado, remover a entrada inteira do array resources
+        if (index !== -1) {
+            this.resources.splice(index, 1);
+            this.cdr.detectChanges(); // Força uma nova detecção de alterações após a alteração no array
+            localStorage.setItem('classCache', JSON.stringify(this.resources));
         }
     }
 }
