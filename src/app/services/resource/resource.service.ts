5e363b7a18532e1ed97b86de20891bb1fa1bf039
AlexandreSantAnaLangunno <alexandreslangunno@gmail.com>
2024-04-26 20:14:12 -0300

    [GKO-21]- Service base criado

--- src/app/services/resource/resource.service.ts
+++ src/app/services/resource/resource.service.ts
@@ -1,9 +1,42 @@
+
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Resource } from '../../models/resource';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ResourceService {
+    private apiUrl: String;
 
-  constructor() { }
+    constructor(private _http: HttpClient) {
+        this.apiUrl = environment.apiUrl;
+    }
+
+    getResource(uuid: string): Observable<any> {
+        let headers = { 'Content-Type': 'application/json', }
+
+        return this._http.get(`${this.apiUrl}/gk/conteudo/${uuid}`, { headers })
+    }
+
+    deleteResource(uuid: string): Observable<any> {
+        let headers = { 'Content-Type': 'application/json', }
+
+        return this._http.delete(`${this.apiUrl}/gk/conteudo/${uuid}`, { headers })
+    }
+
+    postResource(resource: Resource): Observable<any> {
+        let headers = { 'Content-Type': 'application/json', }
+
+        return this._http.post(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+    }
+
+    updateResource(resource: Resource): Observable<any> {
+        let headers = { 'Content-Type': 'application/json', }
+        return this._http.put(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+    }
+
 }
