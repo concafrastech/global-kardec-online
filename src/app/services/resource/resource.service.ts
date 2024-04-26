f10acca40e2f179524e454490a8f6cb89213b74d
AlexandreSantAnaLangunno <alexandreslangunno@gmail.com>
2024-04-26 20:30:34 -0300

    [GKO-21]- Revisão do gpt

--- src/app/services/resource/resource.service.ts
+++ src/app/services/resource/resource.service.ts
@@ -1,42 +1,55 @@
-
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
 import { Resource } from '../../models/resource';
-
 
 @Injectable({
     providedIn: 'root'
 })
 export class ResourceService {
-    private apiUrl: String;
+    private apiUrl: string;
 
-    constructor(private _http: HttpClient) {
+    constructor(private httpClient: HttpClient) {
         this.apiUrl = environment.apiUrl;
     }
 
     getResource(uuid: string): Observable<any> {
-        let headers = { 'Content-Type': 'application/json', }
-
-        return this._http.get(`${this.apiUrl}/gk/conteudo/${uuid}`, { headers })
+        return this.httpClient.get<any>(`${this.apiUrl}/gk/conteudo/${uuid}`)
+            .pipe(
+                catchError(this.handleError)
+            );
     }
 
     deleteResource(uuid: string): Observable<any> {
-        let headers = { 'Content-Type': 'application/json', }
-
-        return this._http.delete(`${this.apiUrl}/gk/conteudo/${uuid}`, { headers })
+        return this.httpClient.delete<any>(`${this.apiUrl}/gk/conteudo/${uuid}`)
+            .pipe(
+                catchError(this.handleError)
+            );
     }
 
     postResource(resource: Resource): Observable<any> {
-        let headers = { 'Content-Type': 'application/json', }
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-        return this._http.post(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+        return this.httpClient.post<any>(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+            .pipe(
+                catchError(this.handleError)
+            );
     }
 
     updateResource(resource: Resource): Observable<any> {
-        let headers = { 'Content-Type': 'application/json', }
-        return this._http.put(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+        return this.httpClient.put<any>(`${this.apiUrl}/gk/conteudo/`, resource, { headers })
+            .pipe(
+                catchError(this.handleError)
+            );
     }
 
+    private handleError(error: any): Observable<any> {
+        // Implemente a lógica de tratamento de erros aqui, como logar o erro ou exibir uma mensagem de erro para o usuário
+        console.error('Ocorreu um erro:', error);
+        throw error;
+    }
 }
