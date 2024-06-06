e8af23f50d31373528cf07052fd927efe66fb04a
victeo <victor.atomo@gmail.com>
2024-06-06 16:15:24 -0300

    [GKO-27] - Add comentários e correção do handleError

--- src/app/services/frequency/frequency.service.ts
+++ src/app/services/frequency/frequency.service.ts
@@ -1,72 +1,85 @@
-import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { Frequency } from '../../models/frequency';
+import {environment} from '../../../environments/environment';
+import {Observable, throwError} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {catchError} from 'rxjs/operators';
+import {Frequency} from '../../models/frequency';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FrequencyService {
-  private apiUrl: string;
+    private apiUrl: string;
 
-  constructor(private httpClient: HttpClient) {
-    this.apiUrl = environment.apiUrl;
-  }
-  /**
- *
- * @param classUuid
- */
-  getFrequencyByClass(classUuid: string): Observable<any> {
-    return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porTurma/${classUuid}`)
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
-  /**
-    *
-    * @param registryUuid
-    */
-  getFrequencyByRegistry(registryUuid: string): Observable<any> {
-    return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porMatricula/${registryUuid}`)
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
-  /**
-      *
-      * @param calendarUuid
-      * @param classDate
-      */
-  getFrequencyByCalendar(calendarUuid: string, classDate: string): Observable<any> {
-    return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porCalendario/${calendarUuid}/${classDate}`)
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+    constructor(private httpClient: HttpClient) {
+        this.apiUrl = environment.apiUrl;
+    }
 
-  /**
- *
- * @param frequency
- */
-  updateResource(frequency: Frequency): Observable<any> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-    return this.httpClient.put<any>(`${this.apiUrl}/gk/frequencia/`, frequency, { headers })
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
-
-  /**
+    /**
      *
-     * @param error
+     * @param classUuid
      */
-  private handleError(error: any): Observable<any> {
-    // Implemente a lógica de tratamento de erros aqui, como logar o erro ou exibir uma mensagem de erro para o usuário
-    console.error('Ocorreu um erro:', error);
-    throw error;
-  }
+    getFrequencyByClass(classUuid: string): Observable<any> {
+        return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porTurma/${classUuid}`)
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    /**
+     *
+     * @param registryUuid
+     */
+    getFrequencyByRegistry(registryUuid: string): Observable<any> {
+        return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porMatricula/${registryUuid}`)
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    /**
+     *
+     * @param calendarUuid
+     * @param classDate
+     */
+    getFrequencyByCalendar(calendarUuid: string, classDate: string): Observable<any> {
+        return this.httpClient.get<any>(`${this.apiUrl}/gk/frequencia/porCalendario/${calendarUuid}/${classDate}`)
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    /**
+     *
+     * @param frequency
+     */
+    updateResource(frequency: Frequency): Observable<any> {
+        const headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+        return this.httpClient.put<any>(`${this.apiUrl}/gk/frequencia/`, frequency, {headers})
+            .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    /**
+     * Lida com erros retornados pelas requisições HTTP.
+     *
+     * @param error O objeto de erro retornado pela requisição HTTP.
+     * @returns Um Observable com uma mensagem de erro.
+     */
+    private handleError(error: any) {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // Ocorreu um erro do lado do cliente ou da rede. Trate-o de acordo.
+            errorMessage = 'Ocorreu um erro: ' + error.error.message;
+        } else {
+            // O backend retornou um código de resposta não bem-sucedido.
+            // O corpo da resposta pode conter pistas sobre o que deu errado.
+            errorMessage = `O backend retornou o código ${error.status}: ${error.message}`;
+        }
+        console.error(errorMessage); // Registre o erro no console
+        return throwError(errorMessage); // Relance o erro como um observable para manipulação adequada de erros
+    }
 }
