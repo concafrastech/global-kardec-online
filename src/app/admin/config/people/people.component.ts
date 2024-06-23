72add3779aecc78daf1ded01fe5c16c2c8353fd1
victeo <victor.atomo@gmail.com>
2024-06-23 13:45:37 -0300

    [GKO-33] - Padronização de estilos

--- src/app/admin/config/people/people.component.ts
+++ src/app/admin/config/people/people.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { RouterLink } from '@angular/router';
+import {CardModule} from "primeng/card";
 
 @Component({
     selector: 'app-people',
     standalone: true,
-    imports: [ButtonModule, RippleModule, RouterLink],
+    imports: [ButtonModule, RippleModule, RouterLink, CardModule],
     templateUrl: './people.component.html',
     styleUrl: './people.component.less',
 })
